1084af0a36a6b2a1f06a077e525ff3b54c196c75
MichaelGranzeier <granzeierpeter@gmail.com>
2023-08-22 20:38:42 -0400

    added searchbar

--- JS/index.js
+++ JS/index.js
@@ -59,34 +59,124 @@ function colorLinks(hex){
 
 //pagination
 let currentPage = 0;
-let rows = 5;
+localStorage.getItem("currentPage", currentPage);
+let rows = 3;
 let pagination = [];
 let articles = [];
 let items = document.getElementById("items").children.length;
+let page = localStorage.getItem("page");
+let pagesAvailable = Math.ceil(items / rows) - 1;
 
+changePage();
 
 function next(){
-    currentPage++;
+    if(currentPage < pagesAvailable){
+     currentPage++;
+    localStorage.setItem("page", currentPage);
+    activePage();
+    changePage();
+    shiftPages();   
+    }else{
+        alert("Can't go further!");
+    }
+}
+
+function prev(){
+        if(currentPage > 0){
+        currentPage--;
+        localStorage.setItem("page", currentPage);
+        activePage();
+        changePage();
+        shiftPages();
+    }else{
+        alert("Can't go further!");
+    }
+}
+
+function selectPage0(){
+    let Page0 = document.getElementById("page0").innerText;
+    let page0 = Page0 - 1;
+    localStorage.setItem("page", page0);
+    currentPage = page0;
     activePage();
     changePage();
     shiftPages();
 }
 
-function prev(){
-    currentPage--;
+function selectPage1(){
+    let Page1 = document.getElementById("page1").innerText;
+    let page1 = Page1 - 1;
+    localStorage.setItem("page", page1);
+    currentPage = page1;
     activePage();
     changePage();
     shiftPages();
+}
+
+function selectPage2(){
+    if(currentPage < pagesAvailable){
+        let Page2 = document.getElementById("page2").innerText;
+        let page2 = Page2 - 1;
+        localStorage.setItem("page", page2);
+        currentPage = page2;
+        activePage();
+        changePage();
+        shiftPages(); 
+    }else{
+        alert("Can't go to this page yet!");
+    }
+}
+
+function selectPage3(){
+    if(currentPage < pagesAvailable){
+        let Page3 = document.getElementById("page3").innerText;
+        let page3 = Page3 - 1;
+        localStorage.setItem("page", page3);
+        currentPage = page3;
+        activePage();
+        changePage();
+        shiftPages();
+    }else{
+        alert("Can't go to this page yet!");
+    }
+}
+
+function selectPage4(){
+    if(currentPage < pagesAvailable){
+        let Page4 = document.getElementById("page4").innerText;
+        let page4 = Page4 - 1;
+        localStorage.setItem("page", page4);
+        currentPage = page4;
+        activePage();
+        changePage();
+        shiftPages(); 
+    }else{
+        alert("Can't go to this page yet!");
+    }
+}
+
+if(page === "0"){
+    selectPage0();
+}else if (page === "1"){
+    selectPage1();
+}else if (page === "2"){
+    selectPage2();
+}else if (page === "3"){
+    selectPage3();
+}else if (page === "4"){
+    selectPage4();
 }
 
 function activePage(){
     if(pagination.length == 0){
         addToPagination();
     }
-    for(var i=0; i<pagination.length; i++){
-        document.getElementById('page' + i).classList.remove('active');
+    if(currentPage < 3){
+        for(var i=0; i<pagination.length; i++){
+            document.getElementById('page' + i).classList.remove('active');
+        }
+        document.getElementById('page' + currentPage).classList.add("active");
     }
-    document.getElementById('page' + currentPage).classList.add("active");
 }
 
 function addToPagination(){
@@ -103,20 +193,41 @@ function changePage(){
     }
     for(var d=0; d<rows; d++){
         let firstArticle = artStart + d;
-        console.log(artStart);
         let displayArticles = document.getElementById("article" + firstArticle);
-        displayArticles.style.display = "block";
+        if(displayArticles !== null){
+            displayArticles.style.display = "block";
+        }
     }
 }
 
-/*function shiftPages(){
+function shiftPages(){
     if(currentPage > 1){
         let active = currentPage + 1;
-        console.log(active)
         document.getElementById("page0").innerHTML = active - 2;
         document.getElementById("page1").innerHTML = active - 1;
-        document.getElementById("page2").innerText = active;
-        document.getElementById("page3").innerText = active + 1;
-        document.getElementById("page4").innerText = active + 2;
+        document.getElementById("page2").innerHTML = active;
+        document.getElementById("page3").innerHTML = active + 1;
+        document.getElementById("page4").innerHTML = active + 2;
     }
-}*/
+}
+
+function search(){
+    let searchItem = document.getElementById("search").value.toUpperCase();
+    let art = document.querySelectorAll('.art');
+    let name = [];
+
+    art.forEach((aname) => {
+        name.push(aname.innerText);
+    });
+
+    for(var v=0; v<name.length; v++){
+        name[v] = name[v].toUpperCase();
+        if(name[v].includes(searchItem) === true){
+            let artic = document.getElementById("article" + v);
+            artic.style.display = "block";
+        }else{
+            let ar = document.getElementById("article" + v);
+            ar.style.display = "none";
+        }
+    }
+}
